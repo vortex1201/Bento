5545471cf819c0e904313d323de9e79ea3144c82
vortex1201 <125912466+vortex1201@users.noreply.github.com>
2023-02-21 07:32:17 +0400

    Update config.js

--- config.js
+++ config.js
@@ -26,14 +26,14 @@ const CONFIG = {
 	bentoLayout: 'buttons', // 'bento', 'lists', 'buttons'
 
 	// Weather
-	weatherKey: 'InsertYourAPIKeyHere123456', // Write here your API Key
+	weatherKey: 'a94eff56481558856c9b5bfbe33850bb', // Write here your API Key
 	weatherIcons: 'OneDark', // 'Onedark', 'Nord', 'Dark', 'White'
 	weatherUnit: 'C', // 'F', 'C'
 	language: 'en', // More languages in https://openweathermap.org/current#multi
 
 	trackLocation: true, // If false or an error occurs, the app will use the lat/lon below
-	defaultLatitude: '37.775',
-	defaultLongitude: '-122.419',
+	defaultLatitude: '25.204849',
+	defaultLongitude: '55.270782',
 
 	// Autochange
 	autoChangeTheme: true,
